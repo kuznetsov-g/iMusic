ea5f8da2c898f56d607397f54c21406e214a64f5
George <backs159@icloud.com>
2023-01-03 12:09:03 -0500

    add Volume and CurrentTime sliders logic

--- iMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -76,12 +76,19 @@ class TrackDetailView: UIView {
             self?.currentTimeLabel.text = time.toDisplayString()
             
             let durationTime = self?.player.currentItem?.duration
-            let currentDurationTimeText = ((durationTime ?? CMTimeMake(value: 1, timescale: 1)) - time).toDisplayString()
+            let currentDurationText = ((durationTime ?? CMTimeMake(value: 1, timescale: 1)) - time).toDisplayString()
             
-            self?.durationLabel.text = "-\(currentDurationTimeText)"
+            self?.durationLabel.text = "-\(currentDurationText)"
+            self?.updateCurrentTimeSlider()
         }
     }
     
+    private func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationSeconds = CMTimeGetSeconds((player.currentItem?.duration ?? CMTimeMake(value: 1, timescale: 1)))
+        let percentage = currentTimeSeconds / durationSeconds
+        self.currentTimeSlider.value = Float(percentage)
+    }
     
     // MARK: - Animations
     
@@ -106,12 +113,20 @@ class TrackDetailView: UIView {
         self.removeFromSuperview()
     }
     @IBAction func handleCurrnetTimeSlider(_ sender: Any) {
+        let percentage = currentTimeSlider.value
+        guard let duration = player.currentItem?.duration else { return }
+        let durationInSeconds = CMTimeGetSeconds(duration)
+        let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+        let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+        player.seek(to: seekTime)
     }
     @IBAction func handleVolumeSlider(_ sender: Any) {
+        player.volume = volumeSlider.value
     }
     @IBAction func previousTrack(_ sender: Any) {
     }
     @IBAction func nextTrack(_ sender: Any) {
+        
     }
     @IBAction func playPauseAction(_ sender: Any) {
         if player.timeControlStatus == .paused {
